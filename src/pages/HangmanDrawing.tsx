dba3abeaaf4d981cd3d05e2e844f167c585ad0ad
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-10-18 15:04:17 +0600

    :bulb: set hangman shape

--- src/pages/HangmanDrawing.tsx
+++ src/pages/HangmanDrawing.tsx
@@ -1,6 +1,126 @@
+// ========== Head ============//
+const Head = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "8px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
+);
+
+// ============== Body ===============//
+const Body = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      background: "black",
+      position: "absolute",
+      right: "0px",
+      top: "108px",
+    }}
+  />
+);
+
+// =============== RightARM ===========/
+const RightARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      right: "-95px",
+      top: "150px",
+      rotate:"-30deg",
+      transformOrigin:"left bottom"
+    }}
+  />
+);
+
+
+// =============== LeftARM ===========/
+const LeftARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      right: "10px",
+      top: "148px",
+      rotate:"30deg",
+      transformOrigin:"right bottom"
+    }}
+  />
+);
+
+
+// =============== RightLeg ===========/
+const RightLeg = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      right: "-90px",
+      top: "190px",
+      rotate:"60deg",
+      transformOrigin:"left bottom"
+    }}
+  />
+);
+
+// =============== LeftLeg ===========/
+const LeftLeg = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      right: "0",
+      top: "190px",
+      rotate:"-60deg",
+      transformOrigin:"right bottom"
+    }}
+  />
+);
+
+
 const HangmanDrawing = () => {
   return (
     <div style={{ position: "relative" }}>
+      {/* ============= head =========== */}
+      {Head}
+      {/* =============== body =========== */}
+      {Body}
+      {/* ============= RightARM =========== */}
+         {RightARM}
+      {/* ================ LeftARM ============= */}
+      {LeftARM}
+     {/* =============== RightLeg ========== */}
+     {RightLeg}
+
+     {/* ======= LeftLeg ======= */}
+     {LeftLeg}
+
+      <div
+        style={{
+          height: "50px",
+          width: "10px",
+          background: "black",
+          position: "absolute",
+          top: 0,
+          right: 0,
+        }}
+      />
       <div
         style={{
           height: "10px",
