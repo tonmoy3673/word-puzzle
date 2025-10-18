1f87eb5cb281b4c19cb423346f4312969eb23a2d
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-10-18 12:33:45 +0600

    :truck: set routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import RootLayout from "./layouts/RootLayout";
+import "./index.css";
+import Home from "./components/Home";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
